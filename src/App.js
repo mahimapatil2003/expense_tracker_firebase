ded67139209c626fd080f78f68cc8205c93e320c
mahimapatil2003 <104967133+mahimapatil2003@users.noreply.github.com>
2024-07-28 21:50:27 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
->>>>>>> 874559f6be27c900e0b6fe911294c7e71d084ef4
 import { Auth } from "./pages/auth/index";
 import { ExpenseTracker } from "./pages/expense-tracker/index";
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter basename="/expense_tracker_firebase">
+      <Router>
         <Routes>
           <Route path="/" exact element={<Auth />} />
           <Route path="/expense-tracker" element={<ExpenseTracker />} />
         </Routes>
-      </BrowserRouter>
+      </Router>
     </div>
   );
 }
