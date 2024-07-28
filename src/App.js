06ff6096e2fd452ebd726c1ac7086eb8201c2e98
mahima <mahimahari533@gmail.com>
2024-07-28 20:11:34 +0530

    add hashrouter

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, HashRouter } from "react-router-dom";
 import { Auth } from "./pages/auth/index";
 import { ExpenseTracker } from "./pages/expense-tracker/index";
 
 function App() {
   return (
     <div className="App">
-      <Router>
+      <HashRouter>
         <Routes>
           <Route path="/" exact element={<Auth />} />
           <Route path="/expense-tracker" element={<ExpenseTracker />} />
         </Routes>
-      </Router>
+      </HashRouter>
     </div>
   );
 }
