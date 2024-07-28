ff6d6fc0424c08f97fb2ca8951d42ff4a1582cde
mahimapatil2003 <104967133+mahimapatil2003@users.noreply.github.com>
2024-07-28 21:49:50 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import "./App.css";
-<<<<<<< HEAD
-
-import { BrowserRouter ,  Route, Routes } from "react-router-dom";
-=======
-import { BrowserRouter as HashRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 >>>>>>> 874559f6be27c900e0b6fe911294c7e71d084ef4
 import { Auth } from "./pages/auth/index";
 import { ExpenseTracker } from "./pages/expense-tracker/index";
